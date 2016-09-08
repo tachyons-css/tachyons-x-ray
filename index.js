489e21f75889d499488fb32a93618c51d916094c
John Otander <johnotander@gmail.com>
2016-09-08 13:22:31 -0600

    Implement initial functionality

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-'use strict'
-
-module.exports = function tachyonsDebugBrowser (options) {
-  options = options || {}
-
-  return true
-}
+chrome.browserAction.onClicked.addListener(tab => {
+  chrome.tabs.executeScript({
+    code: `
+      const style = document.createElement('style')
+      document.head.appendChild(style)
+      style.sheet.insertRule("body { background: transparent url(https://raw.githubusercontent.com/tachyons-css/tachyons-css.github.io/master/img/8-grid-blue-alpha.png) repeat top left !important; }", 0)
+      style.sheet.insertRule("* { background-color: transparent !important; color: #444 !important; }", 1)
+    `
+  })
+})
